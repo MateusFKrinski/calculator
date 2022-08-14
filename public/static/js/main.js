d075f0bc765c0c2b5a6c992453b1772af728bde1
Mateus Francisco Krinski <mateus.krinski10@gmail.com>
2022-08-14 16:18:55 -0300

    first-comit

---
+++ public/static/js/main.js
@@ -0,0 +1,39 @@
+var um = document.getElementById('um');
+var dois = document.getElementById('dois');
+var tres = document.getElementById('tres');
+var quatro = document.getElementById('quatro');
+var cinco = document.getElementById('cinco');
+var seis = document.getElementById('seis');
+var sete = document.getElementById('sete');
+var oito = document.getElementById('oito');
+var nove = document.getElementById('nove');
+var zero = document.getElementById('zero');
+var vezes = document.getElementById('vezes');
+var dividir = document.getElementById('dividir');
+var mais = document.getElementById('mais');
+var menos = document.getElementById('menos');
+var ponto = document.getElementById('ponto');
+var limpa = document.getElementById('limpa');
+var parentesesD = document.getElementById('parenteses-d');
+var parentesesE = document.getElementById('parenteses-e');
+var igual = document.getElementById('resultado');
+
+var visor = document.getElementById('calc-visor');
+
+const calculadora = document.querySelector('#btns-calc');
+const btn = calculadora.querySelector('.calculator__button');
+
+// const painel = [];
+
+
+// let nUm = 1;
+// let nDois = 2;
+// let nTres = 3;
+// let nQuatro = 4;
+// let nCinco = 5;
+// let nSeis = 6;
+// let nSete = 7;
+// let nOito = 8;
+// let nNove = 9;
+// let nZero = 0;
+
